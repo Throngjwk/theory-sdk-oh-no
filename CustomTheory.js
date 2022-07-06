5fc9a13ae9f137cd71331a52f9b8330049cc451f
Throngjwk <105552423+Throngjwk@users.noreply.github.com>
2022-07-06 09:10:45 -0300

    wow

--- CustomTheory.js
+++ CustomTheory.js
@@ -11,11 +11,18 @@ var authors = "Gilles-Philippe Paillé";
 var version = 1;
 
 var currency;
-var c1, c2;
-var c1Exp, c2Exp;
-
-var achievement1, achievement2;
-var chapter1, chapter2;
+var Math1 = ["a_1", "a_2", "a_3", "a_4", "a_5", "a_6", "a_7", "a_8"]
+var cost = [
+    20,
+    300,
+    4000,
+    50000,
+    6e5,
+    7e6,
+    8e7,
+    9e8,
+]
+var array = new Array(8)
 
 var init = () => {
     currency = theory.createCurrency();
@@ -23,21 +30,15 @@ var init = () => {
     ///////////////////
     // Regular Upgrades
 
-    // c1
-    {
-        let getDesc = (level) => "c_1=" + getC1(level).toString(0);
-        c1 = theory.createUpgrade(0, currency, new FirstFreeCost(new ExponentialCost(15, Math.log2(2))));
-        c1.getDescription = (_) => Utils.getMath(getDesc(c1.level));
-        c1.getInfo = (amount) => Utils.getMathTo(getDesc(c1.level), getDesc(c1.level + amount));
-    }
+    // All 8 A Vars
 
-    // c2
-    {
-        let getDesc = (level) => "c_2=2^{" + level + "}";
-        let getInfo = (level) => "c_2=" + getC2(level).toString(0);
-        c2 = theory.createUpgrade(1, currency, new ExponentialCost(5, Math.log2(10)));
-        c2.getDescription = (_) => Utils.getMath(getDesc(c2.level));
-        c2.getInfo = (amount) => Utils.getMathTo(getInfo(c2.level), getInfo(c2.level + amount));
+    for (let i = 0; i < 8; i++) {
+        {
+            let getDesc = (level) => "q_{" + i + "}" + getC1(level).toString(0);
+            c1 = theory.createUpgrade(i, currency, new FirstFreeCost(new ExponentialCost(15, Math.log2(2))));
+            c1.getDescription = (_) => Utils.getMath(getDesc(c1.level));
+            c1.getInfo = (amount) => Utils.getMathTo(getDesc(c1.level), getDesc(c1.level + amount));
+        }
     }
 
     /////////////////////
@@ -50,35 +51,18 @@ var init = () => {
     //// Milestone Upgrades
     theory.setMilestoneCost(new LinearCost(25, 25));
 
-    {
-        c1Exp = theory.createMilestoneUpgrade(0, 3);
-        c1Exp.description = Localization.getUpgradeIncCustomExpDesc("c_1", "0.05");
-        c1Exp.info = Localization.getUpgradeIncCustomExpInfo("c_1", "0.05");
-        c1Exp.boughtOrRefunded = (_) => theory.invalidatePrimaryEquation();
-    }
-
-    {
-        c2Exp = theory.createMilestoneUpgrade(1, 3);
-        c2Exp.description = Localization.getUpgradeIncCustomExpDesc("c_2", "0.05");
-        c2Exp.info = Localization.getUpgradeIncCustomExpInfo("c_2", "0.05");
-        c2Exp.boughtOrRefunded = (_) => theory.invalidatePrimaryEquation();
-    }
     
     /////////////////
     //// Achievements
-    achievement1 = theory.createAchievement(0, "Achievement 1", "Description 1", () => c1.level > 1);
-    achievement2 = theory.createSecretAchievement(1, "Achievement 2", "Description 2", "Maybe you should buy two levels of c2?", () => c2.level > 1);
 
     ///////////////////
     //// Story chapters
-    chapter1 = theory.createStoryChapter(0, "My First Chapter", "This is line 1,\nand this is line 2.\n\nNice.", () => c1.level > 0);
-    chapter2 = theory.createStoryChapter(1, "My Second Chapter", "This is line 1 again,\nand this is line 2... again.\n\nNice again.", () => c2.level > 0);
 
     updateAvailability();
 }
 
 var updateAvailability = () => {
-    c2Exp.isAvailable = c1Exp.level > 0;
+    //
 }
 
 var tick = (elapsedTime, multiplier) => {
@@ -91,15 +75,7 @@ var tick = (elapsedTime, multiplier) => {
 var getPrimaryEquation = () => {
     let result = "\\dot{\\rho} = c_1";
 
-    if (c1Exp.level == 1) result += "^{1.05}";
-    if (c1Exp.level == 2) result += "^{1.1}";
-    if (c1Exp.level == 3) result += "^{1.15}";
-
     result += "c_2";
-
-    if (c2Exp.level == 1) result += "^{1.05}";
-    if (c2Exp.level == 2) result += "^{1.1}";
-    if (c2Exp.level == 3) result += "^{1.15}";
 
     return result;
 }
